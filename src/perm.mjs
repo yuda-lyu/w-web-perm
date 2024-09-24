4ad097d249e5f4e0e7c4d363cbea5ea8d2b45b5f
yuda-lyu <firsemisphere@gmail.com>
2024-09-24 16:53:24 +0800

    modify: perm

--- src/perm.mjs
+++ src/perm.mjs
@@ -2,7 +2,6 @@ import axios from 'axios'
 import get from 'lodash-es/get.js'
 import each from 'lodash-es/each.js'
 import isestr from 'wsemi/src/isestr.mjs'
-import isbol from 'wsemi/src/isbol.mjs'
 import isfun from 'wsemi/src/isfun.mjs'
 import genPm from 'wsemi/src/genPm.mjs'
 import pmInvResolve from 'wsemi/src/pmInvResolve.mjs'
@@ -28,12 +27,6 @@ function perm() {
             throw new Error(`url does not include 'userId='`)
         }
 
-        //useAsync
-        let useAsync = get(opt, 'useAsync')
-        if (!isbol(useAsync)) {
-            useAsync = false
-        }
-
         //permSuccess
         let permSuccess = get(opt, 'permSuccess')
 
@@ -41,10 +34,7 @@ function perm() {
         let permError = get(opt, 'permError')
 
         //pm
-        let pm = null
-        if (useAsync) {
-            pm = genPm()
-        }
+        let pm = genPm()
 
         //get
         let pms = axios.get(url)
@@ -56,24 +46,17 @@ function perm() {
                 if (isfun(permSuccess)) {
                     permSuccess(pmd)
                 }
-                if (useAsync) {
-                    pm.resolve(kp)
-                }
+                pm.resolve(pmd)
             })
             .catch((err) => {
                 // console.log('perm.getPerm catch', err)
                 if (isfun(permError)) {
                     permError(err)
                 }
-                if (useAsync) {
-                    pm.reject()
-                }
+                pm.reject(err)
             })
 
-        if (useAsync) {
-            return pm
-        }
-        return kp
+        return pm
     }
 
     //user
